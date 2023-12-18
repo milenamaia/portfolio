176f19dbbd6927a2c87a5b6ea0314392900da116
milenamaia <milenamaiadesouza123@gmail.com>
2023-12-18 16:16:08 -0300

    atualizando

--- javascript.js
+++ javascript.js
@@ -6,3 +6,38 @@ menu.addEventListener('click', () => {
     NavMenu.classList.toggle('ativo');
 })
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e) {
+        e.preventDefault();
+        toggleMenu();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+});
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.item-menu' , {duration: 2000});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.querySelector('.navigation');
+    const menu = document.querySelector('.nav-menu');
+
+    hamburgerBtn.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
