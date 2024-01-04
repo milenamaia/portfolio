12420530ab97d9cc52903f5eb246277eb5c268d4
milenamaia <milenamaiadesouza123@gmail.com>
2024-01-04 20:09:48 -0300

    arrumando menu

--- javascript.js
+++ javascript.js
@@ -13,3 +13,22 @@
         const elements = document.querySelectorAll('.hidden')
 
         elements.forEach((element) => myObserver.observe(element))
+
+        
+        document.addEventListener('DOMContentLoaded', function() {
+            const hamburgerBtn = document.querySelector('.navigation');
+            const menu = document.querySelector('.nav-menu');
+        
+            hamburgerBtn.addEventListener('click', function() {
+                menu.classList.toggle('active');
+            });
+        });
+        
+        const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+
