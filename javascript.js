10cf889bd92f466753e56940fb8d9dc3051e3486
milenamaia <milenamaiadesouza123@gmail.com>
2024-01-03 21:42:00 -0300

    atualizando

--- javascript.js
+++ javascript.js
@@ -1,25 +1,15 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
+       
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-});
+        const myObserver = new IntersectionObserver((entries) => {
+             entries.forEach((entry) =>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add('show')
+                } else {
+                    entry.target.classList.remove('show')
+                }
+             })
+        })
 
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerBtn = document.querySelector('.navigation');
-    const menu = document.querySelector('.nav-menu');
+        const elements = document.querySelectorAll('.hidden')
 
-    hamburgerBtn.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
-
-
-var $doc = $('html, body');
-        $('a').click(function() {
-            $doc.animate({
-                scrollTop: $( $.attr(this, 'href')).offset().top
-            }), 10;
-            return false;
-        });
+        elements.forEach((element) => myObserver.observe(element))
--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-    
-    var $doc = $('html, body');
-        $('a').click(function() {
-            $doc.animate({
-                scrollTop: $( $.attr(this, 'href')).offset().top
-            }), 10;
-            return false;
-        });
